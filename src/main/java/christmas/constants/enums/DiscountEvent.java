34f49284e511acd8a83bb152eebe617d340fca47
ch1hyun <och2510@gmail.com>
2023-11-14 16:37:54 +0900

    feat: 할인 이벤트 정의
    - DiscountEvent.java
    - Week.java

---
+++ src/main/java/christmas/constants/enums/DiscountEvent.java
@@ -0,0 +1,30 @@
+package christmas.constants.enums;
+
+public enum DiscountEvent {
+    디데이(Week.디데이, 1_000, 100), // 기간, 시작 금액, 증가 금액
+    평일(Week.평일, 2_023, 2_023),
+    주말(Week.주말, 2_023, 2_023),
+    특별(Week.특별, 1_000, 0);
+
+    private final Week week;
+    private final Integer startAmount;
+    private final Integer offsetAmount;
+
+    DiscountEvent(Week week, Integer startAmount, Integer offsetAmount) {
+        this.week = week;
+        this.startAmount = startAmount;
+        this.offsetAmount = offsetAmount;
+    }
+
+    public Boolean contains(Day day) {
+        return week.contains(day);
+    }
+
+    public Integer getStartAmount() {
+        return startAmount;
+    }
+
+    public Integer getOffsetAmount() {
+        return offsetAmount;
+    }
+}
--- src/main/java/christmas/constants/enums/Week.java
+++ src/main/java/christmas/constants/enums/Week.java
@@ -6,7 +6,8 @@ import java.util.List;
 public enum Week {
     평일(Arrays.asList(Day.일, Day.월, Day.화, Day.수, Day.목)),
     주말(Arrays.asList(Day.금, Day.토)),
-    특별(Arrays.asList(Day.일, Day.크리스마스));
+    특별(Arrays.asList(Day.일, Day.크리스마스)),
+    디데이(Arrays.asList(Day.첫날, Day.크리스마스));
 
     private final List<Day> dayList;
 
@@ -14,7 +15,7 @@ public enum Week {
         this.dayList = dayList;
     }
 
-    public List<Day> getDayList() {
-        return dayList;
+    public Boolean contains(Day day) {
+        return dayList.contains(day);
     }
 }
