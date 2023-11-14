c21225878cddf1f6a04c5fcd0cfca1af47eee77e
ch1hyun <och2510@gmail.com>
2023-11-15 01:12:56 +0900

    refactor: Result Controller 제거
    - EventPlannerController.java

--- src/main/java/christmas/controller/EventPlannerController.java
+++ src/main/java/christmas/controller/EventPlannerController.java
@@ -1,24 +1,18 @@
 package christmas.controller;
 
-import christmas.model.OrderAmount;
-import christmas.model.OrderDate;
-import christmas.model.OrderList;
-
 public class EventPlannerController implements EntryController {
     private final OrderController orderController;
     private final EventController eventController;
     private final CalculateController calculateController;
-    private final ResultController resultController;
 
-    private EventPlannerController(OrderController orderController, EventController eventController, CalculateController calculateController, ResultController resultController) {
+    private EventPlannerController(OrderController orderController, EventController eventController, CalculateController calculateController) {
         this.orderController = orderController;
         this.eventController = eventController;
         this.calculateController = calculateController;
-        this. resultController = resultController;
     }
 
-    public static EventPlannerController of(OrderController orderController, EventController eventController, CalculateController calculateController, ResultController resultController) {
-        return new EventPlannerController(orderController, eventController, calculateController, resultController);
+    public static EventPlannerController of(OrderController orderController, EventController eventController, CalculateController calculateController) {
+        return new EventPlannerController(orderController, eventController, calculateController);
     }
 
     @Override
