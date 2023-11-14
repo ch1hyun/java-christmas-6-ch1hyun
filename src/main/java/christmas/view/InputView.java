df079ddd9729b0cd96d0d101e5fa814d44924266
ch1hyun <och2510@gmail.com>
2023-11-14 13:14:35 +0900

    feat: 입력 뷰 생성
    - InputView.java

---
+++ src/main/java/christmas/view/InputView.java
@@ -0,0 +1,17 @@
+package christmas.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    private InputView() {}
+
+    public static String readDate() {
+        // Output view 추가
+        return Console.readLine();
+    }
+
+    public static String readMenu() {
+        // Output view 추가
+        return Console.readLine();
+    }
+}
