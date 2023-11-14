a04235130610d41444adf0c66036a34d3fe7999f
ch1hyun <och2510@gmail.com>
2023-11-14 23:05:08 +0900

    feat: 이벤트 활성화/비활성화 모델 생성
    - Activation.java

---
+++ src/main/java/christmas/model/event/Activation.java
@@ -0,0 +1,17 @@
+package christmas.model.event;
+
+public class Activation {
+    private final Boolean active;
+
+    private Activation(Boolean active) {
+        this.active = active;
+    }
+
+    public static Activation from(Boolean active) {
+        return new Activation(active);
+    }
+
+    public Boolean isActivate() {
+        return active;
+    }
+}
