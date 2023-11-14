c9ab4fb02aba2308e2e6165cf0aa76e3da0f88b8
ch1hyun <och2510@gmail.com>
2023-11-14 11:17:46 +0900

    feat: 결과 및 진행 상황 출력 메시지 작성 (static final)
    - OutputMessage.java

---
+++ src/main/java/christmas/constants/OutputMessage.java
@@ -0,0 +1,22 @@
+package christmas.constants;
+
+public class OutputMessage {
+    private OutputMessage() {}
+
+    public static final String MINUS = "-";
+    public static final String START_SYSTEM = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+    public static final String START_RESULT = "12월 3일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
+    public static final String TITLE_ORDER_LIST = "<주문 메뉴>";
+    public static final String TITLE_TOTAL_AMOUNT_BEFORE_DISCOUNT = "<할인 전 총주문 금액>";
+    public static final String TITLE_TOTAL_AMOUNT_AFTER_DISCOUNT = "<할인 후 예상 결제 금액>";
+    public static final String TITLE_PRESENTATION_ITEM = "<증정 메뉴>";
+    public static final String TITLE_REWARD_LIST = "<혜택 내역>";
+    public static final String TITLE_REWARD_AMOUNT = "<총혜택 금액>";
+    public static final String TITLE_EVENT_BADGE = "<12월 이벤트 배지>";
+
+    /* Format Strings */
+    public static final String FORMAT_DECIMAL_AMOUNT = "###,###";
+    public static final String FORMAT_ORDER_ITEM = "%s %d개";
+    public static final String FORMAT_AMOUNT = "%s원";
+    public static final String FORMAT_REWARD_ITEM = "%s: %s";
+}
