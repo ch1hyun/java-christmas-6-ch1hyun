3ed2c5b4e5ccbc9ff2575e39a26f7ef916e17949
ch1hyun <och2510@gmail.com>
2023-11-14 17:43:19 +0900

    style: DiscountEvent.java 이름 수정
    - DiscountEvent.java -> DiscountType.java
    
    이벤트라는 이름이 포괄적이기 때문에 할인 타입으로 수정.

--- src/main/java/christmas/constants/enums/DiscountType.java
+++ src/main/java/christmas/constants/enums/DiscountType.java
@@ -1,6 +1,6 @@
 package christmas.constants.enums;
 
-public enum DiscountEvent {
+public enum DiscountType {
     디데이(Week.디데이, 1_000, 100), // 기간, 시작 금액, 증가 금액
     평일(Week.평일, 2_023, 2_023),
     주말(Week.주말, 2_023, 2_023),
@@ -10,7 +10,7 @@ public enum DiscountEvent {
     private final Integer startAmount;
     private final Integer offsetAmount;
 
-    DiscountEvent(Week week, Integer startAmount, Integer offsetAmount) {
+    DiscountType(Week week, Integer startAmount, Integer offsetAmount) {
         this.week = week;
         this.startAmount = startAmount;
         this.offsetAmount = offsetAmount;
