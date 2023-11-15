46826fcae0115be8605cca581286b7b058b5077b
ch1hyun <och2510@gmail.com>
2023-11-15 20:27:04 +0900

    test: 주말 이벤트 모델 테스트 생성
    - WeekendEventTest.java

--- src/test/java/christmas/model/event/WeekendEventTest.java
+++ src/test/java/christmas/model/event/WeekendEventTest.java
@@ -12,53 +12,53 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-public class WeekdayEventTest {
-    private static final OrderDate object = OrderDate.from(20);
-    private static final OrderDate subject = OrderDate.from(23);
+public class WeekendEventTest {
+    private static final OrderDate object = OrderDate.from(9);
+    private static final OrderDate subject = OrderDate.from(25);
 
     static Stream<Arguments> orderDateWithExpectedBool() {
         return Stream.of(
                 Arguments.arguments(object, EventConstant.TRUE),
                 Arguments.arguments(subject,EventConstant.FALSE),
-                Arguments.arguments(OrderDate.from(25), EventConstant.TRUE)
+                Arguments.arguments(OrderDate.from(31), EventConstant.TRUE)
         );
     }
 
     @ParameterizedTest
     @MethodSource("orderDateWithExpectedBool")
-    @DisplayName("평일 이벤트는 해당 날짜가 평일일 경우에만 활성화 됩니다.")
-    void 평일_이벤트_날짜_테스트(OrderDate orderDate, Boolean expected) {
+    @DisplayName("주말 이벤트는 해당 날짜가 주말일 경우에만 활성화 됩니다.")
+    void 주말_이벤트_날짜_테스트(OrderDate orderDate, Boolean expected) {
         // given
-        WeekdayEvent weekdayEvent = WeekdayEvent.from(orderDate);
+        WeekendEvent weekendEvent = WeekendEvent.from(orderDate);
 
-        Assertions.assertThat(weekdayEvent.isAcitve()) // when
+        Assertions.assertThat(weekendEvent.isAcitve()) // when
                 .isEqualTo(expected); // then
     }
 
     @Test
-    @DisplayName("평일 이벤트 타입을 반환해야 합니다.")
+    @DisplayName("주말 이벤트 타입을 반환해야 합니다.")
     void 할인_이벤트_반환_타입_테스트() {
         //given
-        WeekdayEvent weekdayEvent = WeekdayEvent.from(object);
+        WeekendEvent weekendEvent = WeekendEvent.from(object);
 
-        Assertions.assertThat(weekdayEvent.getDiscountType()) // when
-                .isEqualTo(DiscountType.평일); // then
+        Assertions.assertThat(weekendEvent.getDiscountType()) // when
+                .isEqualTo(DiscountType.주말); // then
     }
 
     @Test
-    @DisplayName("평일 이벤트는 디저트 메뉴판을 반환해야 합니다.")
+    @DisplayName("주말 이벤트는 메인 메뉴판을 반환해야 합니다.")
     void 메뉴판_반환_타입_테스트() {
         //given
-        WeekdayEvent weekdayEvent = WeekdayEvent.from(object);
+        WeekendEvent weekendEvent = WeekendEvent.from(object);
 
-        Assertions.assertThat(weekdayEvent.getMenuBoard()) // when
-                .isEqualTo(MenuBoard.디저트); // then
+        Assertions.assertThat(weekendEvent.getMenuBoard()) // when
+                .isEqualTo(MenuBoard.메인); // then
     }
 
     static Stream<Arguments> orderDateAndCountWithExpectedString() {
         return Stream.of(
-                Arguments.arguments(object, 4, "평일 할인: -8,092원"),
-                Arguments.arguments(subject, 11, "")
+                Arguments.arguments(object, 3, "주말 할인: -6,069원"),
+                Arguments.arguments(subject, 2, "")
         );
     }
 
@@ -67,9 +67,9 @@ public class WeekdayEventTest {
     @DisplayName("이벤트 활성화 여부에 따라 반환 값이 달라야 합니다. 활성화인 경우 플래너가 요구하는 형식으로 출력되어야 합니다.")
     void 출력_형식_테스트(OrderDate orderDate, Integer count, String expected) {
         // given
-        WeekdayEvent weekdayEvent = WeekdayEvent.from(orderDate);
+        WeekendEvent weekendEvent = WeekendEvent.from(orderDate);
 
-        Assertions.assertThat(weekdayEvent.toString(count)) // when
+        Assertions.assertThat(weekendEvent.toString(count)) // when
                 .isEqualTo(expected); // then
     }
 }
